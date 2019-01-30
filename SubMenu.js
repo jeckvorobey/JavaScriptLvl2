2c711b4869a5cbe51b44c6b82e5834a2d1433338
Jeckvorobey <jeckvorobey69@gmail.com>
2019-01-30 23:14:27 +0300

    Исправил недочеты в ДЗ и привел в порядок код

--- SubMenu.js
+++ SubMenu.js
@@ -1,6 +1,6 @@
 class SubMenu extends MenuItem {
     constructor(href, title) {
-    super (href, title);
+       super(href, title);
     }
     render(){
         return `<li><a href="${this.href}">${this.title}</a></li>`;
