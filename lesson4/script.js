2564e5f579cba7561d94611c6a63c76d6ff324b7
Jeckvorobey <jeckvorobey69@gmail.com>
2019-02-09 22:45:04 +0300

    добавил файлы для выполнения дз к 4 лекции

---
+++ lesson4/script.js
@@ -0,0 +1,35 @@
+//Вкладки TAB
+let $tab = $('tab');
+let $tabContent = $('.tabContent');
+
+let $citySelect = $('#city');
+let $searchSity = $('#cityList')
+
+$(document).ready(() => {
+    $.ajax({
+        type: "get",
+        url: "../lesson4/Cities.json",
+        dataType: "json",
+        success: (data) => {
+            for (let i of data) {
+                let city = $(`<option value="${i.City}">${i.City} \(${i.Region}\)</option>`);
+                let searchSity = $(`<option value="${i.City} \(${i.Region}\)"></option>`);
+                $citySelect.append(city);
+                $searchSity.append(searchSity);
+            }
+        },
+        error: () => {
+            alert('файл Cities.json не найден');
+        }
+    });
+
+    //маска телефона 
+    $('#phone').mask('+7(000)000-00-00', {
+        translation: {
+            '+7': {
+                pattern: /\+[0-9]{1,3}/,
+                optional: true
+            }
+        }
+    })
+});
